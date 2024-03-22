5661f6b418edf882fe5bcbc86c422f543f36e688
leeinwon <inwon.private@icloud.com>
2024-03-22 16:53:35 +0900

    인텔리제이 소스파일 업로드

---
+++ scheduloper/src/main/java/com/example/scheduloper/ScheduloperApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduloperApplication.class, args);
+    }
+
+}
